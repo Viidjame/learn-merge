7c9d778857e5872970f27dab3a6e909c991218c4
Me Thyvidijame <jameleo7777@gmail.com>
2023-09-28 23:31:46 +0700

    Structure

---
+++ src/main/java/com/example/learnmerge/LearnMergeApplication.java
@@ -0,0 +1,15 @@
+package com.example.learnmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMergeApplication.class, args);
+		System.out.println("Hello");
+		System.out.println("Jimmy");
+	}
+
+}
